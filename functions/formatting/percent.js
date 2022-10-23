05d3a002617262e0d98733b8cd08e90f5b2e85cb
David Craig <david@dcraig.dev>
2022-10-24 00:40:24 +0100

    Use 2 decimals by default for percent.

--- functions/formatting/percent.js
+++ functions/formatting/percent.js
@@ -1,8 +1,8 @@
-const percent = (value) => {
+const percent = (value, decimals = 2) => {
   if (typeof value !== "number") {
     return value
   }
-  return `${value}%`
+  return `${value.toFixed(decimals)}%`
 }
 
 export default percent
